bbb28ceac695299a0d7492733cf17aa299ea3d55
w2many <rustamgrankin2009@gmail.com>
2026-08-14 21:03:02 +0300

    add async decorator

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import inspect
+import asyncio
+from typing import Callable
 
 def Hello():
     print("hello!!!")
@@ -9,3 +12,17 @@ def get_users():
     return [{"id": 1, "name": "user"},
             {"id": 2, "name": "user2"},
             {"id": 3, "name": "user3"}]
+
+
+def deco(func: Callable):
+    async def wrapper(*args, **kwargs):
+        call = await func(*args, **kwargs)
+        return call
+    return wrapper
+
+@deco
+async def sum(a: int, b: int):
+    return a+b
+
+result = asyncio.run(sum(5,4))
+print(result)
