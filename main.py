2dce6d7491fc496194ac802f8fa9e42648ffe1e2
w2many <rustamgrankin2009@gmail.com>
2026-08-14 21:05:27 +0300

    add sync decorators support

--- main.py
+++ main.py
@@ -15,14 +15,28 @@ def get_users():
 
 
 def deco(func: Callable):
-    async def wrapper(*args, **kwargs):
-        call = await func(*args, **kwargs)
-        return call
-    return wrapper
+    if inspect.iscoroutinefunction(func):
+        async def wrapper(*args, **kwargs):
+            print("this is async decorator")
+            call = await func(*args, **kwargs)
+            return call
+        return wrapper
+    else:
+        def wrapper(*args, **kwargs):
+            print("this is sync decorator")
+            call = func(*args, **kwargs)
+            return call
+        return wrapper
 
 @deco
 async def sum(a: int, b: int):
     return a+b
 
+@deco
+def multiply(a: int, b: int):
+    return a*b
+
 result = asyncio.run(sum(5,4))
 print(result)
+
+print(multiply(1,7))
