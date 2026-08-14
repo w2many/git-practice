c632d71e2b6b4ce2b87ad2a399eed1742b3ed645
w2many <rustamgrankin2009@gmail.com>
2026-08-14 22:30:43 +0300

    Revert "test"
    This reverts commit 03abdb86c2402f1da9cfb66a7daa034ed3d61b5c.

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def get_users():
             {"id": 2, "name": "user2"},
             {"id": 3, "name": "user3"}]
 
-#some code
 
 def repeat(times: int):
     def decorator(func: Callable):
@@ -68,17 +67,3 @@ result = asyncio.run(sum(5,4))
 print(result)
 
 print(multiply(1,7))
-
-
-print("just test merge")
-
-
-
-
-
-
-print("yoyoyoyo")
-print("yoyoyoyo")
-print("yoyoyoyo")
-print("yoyoyoyo")
-print("stop")
